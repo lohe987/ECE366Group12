550188d41cf4143998784c0c3022d6b20804211f
mdritl2 <mdritl2@uic.edu>
2018-10-10 20:18:06 -0500

    Added disassembler machine code descriptions

--- disassembler.py
+++ disassembler.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
 
 input_file = open("LIS_machine_code.txt", "r")
-output_file = open("program2_disassembled.lis", "w")
+output_file = open("program1_disassembled.lis", "w")
+
+output = "\n"
 
 for line in input_file:
+	print output
+	output_file.write(output)
+
 	wrongOp = False
 	if (line == "\n"):
 		continue
@@ -14,17 +19,17 @@ for line in input_file:
 	op_bin = line[1:8]
 	if (op_bin == "0000000"):
 		op = "HLT"
-		output_file.write(op + "\n")
+		output = op + " //Stop program\n" 
 		continue
 
 	elif (op_bin == "1111100"):
 		op = "ADDN"
-		output_file.write(op + "\n")
+		output = op + "//r3 = r3 - 1\n" 
 		continue
 
 	elif (op_bin == "0001000"):
 		op = "CNTR0"
-		output_file.write(op + "\n")
+		output = op + " //Count the number of 1's in r0\n"
 		continue
 	
 	op_bin = line[1:6]
@@ -35,7 +40,7 @@ for line in input_file:
 		if (ry == '1'):
 			wrongOp = True
 		if (wrongOp == False):
-			output_file.write(op + " r" + ry + "\n")
+			output = op + " r" + ry + " //r0 = r0 - ry\n"
 			continue
 
 	op_bin = line[1:5]
@@ -44,14 +49,14 @@ for line in input_file:
 		op = "XOR"
 		rx = str(int(line[5:6], 2))
 		ry = str(int(line[6:8], 2)) 
-		output_file.write(op + " r" + rx + ", r" + ry + "\n")
+		output = op + " r" + rx + ", r" + ry + " //rx = rx XOR ry\n"
 		continue
 
 	elif (op_bin == "0000"):
 		op = "SLER"
 		rx = str(int(line[5:7], 2))
 		ry = str(int(line[7], 2))
-		output_file.write(op + " r" + rx + ", r" + ry + "\n")
+		output = op + " r" + rx + ", r" + ry + " //If rx < r0 then r3 = 1\n\t//Else r3 = 0\n"
 		continue
 
 	op_bin = line[1:4]
@@ -60,42 +65,42 @@ for line in input_file:
 		op = "ADD"	
 		rx = str(int(line[4:6], 2))
 		ry = str(int(line[6:8], 2))
-		output_file.write(op + " r" + rx + ", " + ry + "\n")
+		output = op + " r" + rx + ", " + ry + " //rx = rx + ry\n"
 		continue
 
 	elif (op_bin == "111"):
 		op = "ADDI"
 		rx = str(int(line[4:6], 2))
 		const = str(int(line[6:8], 2))
-		output_file.write(op + " r" + rx + ", " + const + "\n")
+		output = op + " r" + rx + ", " + const + " //rx = rx + imm\n"
 		continue
 
 	elif (op_bin == "001"):
 		op = "LWD"
 		rx = str(int(line[4:6], 2))
 		ry = str(int(line[6:8], 2))
-		output_file.write(op + " r" + rx + ", r" + ry + "\n")
+		output = op + " r" + rx + ", r" + ry + " //rx = M[ry]\n"
 		continue
 
 	elif (op_bin == "011"):
 		op = "SWD"
 		rx = str(int(line[4:6], 2))
 		ry = str(int(line[6:8], 2))
-		output_file.write(op + " r" + rx + ", r" + ry + "\n")
+		output = op + " r" + rx + ", r" + ry + " //M[ry] = rx\n"
 		continue
 
 	elif (op_bin == "110"):
 		op = "SLE"
 		rx = str(int(line[4:6], 2))
 		ry = str(int(line[6:8], 2))
-		output_file.write(op + " r" + rx + ", " + ry + "\n")
+		output = op + " r" + rx + ", " + ry + " //If rx < ry then r3 = 1\n\t//Else r3 = 0\n"
 		continue
 
 	elif (op_bin == "101"):
 		op = "INIT"
 		rx = str(int(line[4:6], 2))
 		const = str(int(line[6:8], 2))
-		output_file.write(op + " r" + rx + ", " + const + "\n")
+		output = op + " r" + rx + ", " + const + " //rx = imm\n"
 		continue
 	
 	elif (op_bin == "010"):
@@ -106,7 +111,9 @@ for line in input_file:
 			const = -(0b111 - int(const) + 1)
 
 		const = str(const)
+
+		output = op + " " + const + " //If r3 = 1 then jump (PC = PC + imm)\n\t//Else do nothing\n"
 		
-		output_file.write(op + " " + const + "\n")
-		
+	print output
+	output_file.write(output)
 
